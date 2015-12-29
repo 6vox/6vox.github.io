a2c629bca0e3b28f7024a23946a913adde5313f7
toshima66 <toshima66@6vox.com>
2015-12-30 07:36:25 +0900

    issue#27: 位置などを微調整

--- onenga/2016/js/main.js
+++ onenga/2016/js/main.js
@@ -1,5 +1,10 @@
 ;(function ( $, window, document, undefined ) {
 $(function(){
+  //位置を調整する
+  var h = $(window).outerHeight();
+  $('.qr-icon').css({ 'max-height': h/3+'px', 'padding': h/10+'px' });
+  $('.full_member').css({ 'max-height': h+'px', 'padding-top': (h/2-$('.full_member').outerHeight()/1.8)+'px' });
+  $('.company-logo').css({ 'padding-top': (h/2-$('.company-logo').outerHeight()/2)+'px' });
 
   // タイムライン
   var tl_sp = new TimelineMax({delay:0.5, repeat:0, repeatDelay:1});
@@ -18,7 +23,7 @@ $(function(){
 
     .to(".full_member", 6, {opacity: "1"},qr_icon_time+12)
     .to(".full_member", 3, {opacity: "0"},qr_icon_time+18)
-    .to(".campany-logo", 4, {opacity: "1"},qr_icon_time+20)
+    .to(".company-logo", 4, {opacity: "1"},qr_icon_time+20)
 
 });
 })(jQuery, window, document);
