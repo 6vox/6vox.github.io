e6827787e073f4466f116c62c02a5e838f1870fb
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2018-05-15 15:52:52 +0900

    Add office list

--- gatsby-site/src/pages/Outline.js
+++ gatsby-site/src/pages/Outline.js
@@ -3,14 +3,12 @@ import Main from '../components/Main'
 import Table from '../components/Table'
 import TableBody from '../components/TableBody'
 import CompanyInfo from '../components/CompanyInfo'
-import BranchOffice from '../components/BranchOffice'
+import BranchOfficeList from '../components/BranchOfficeList'
 
 const Outline = ({ data }) => (
   <Main id="outline" title="Outline">
     <CompanyInfo info={data.site.siteMetadata.company.info} />
-    {data.site.siteMetadata.company.branchOffices.map((office, index) => (
-      <BranchOffice key={index} {...office} />
-    ))}
+    <BranchOfficeList offices={data.site.siteMetadata.company.branchOffices} />
   </Main>
 )
 
